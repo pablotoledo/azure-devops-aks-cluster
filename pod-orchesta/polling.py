6fb43f575f6a66562f0a489f5e020c1386019e32
Pablo Toledo <pablotoledo@users.noreply.github.com>
2023-10-20 18:20:28 +0200

    polling python
    creando polling.py mejorado

--- pod-orchesta/polling.py
+++ pod-orchesta/polling.py
@@ -1,31 +1,136 @@
+import os
 import requests
+import base64
 from kubernetes import client, config
 import time
-import os
+from tabulate import tabulate
+from termcolor import colored
 
-# Configuración
-ADO_PAT = os.environ.get('ADO_PAT', 'default_value_if_not_provided') 
-AZDO_URL = 'https://dev.azure.com/tu_organizacion/tu_proyecto/_apis/build/builds?api-version=6.0'
-POLLING_INTERVAL = 60  # En segundos
+# Configuración de Kubernetes
 MAX_REPLICAS = 10
 NAMESPACE = 'tu-namespace'
 DEPLOYMENT_NAME = 'nombre-de-tu-deployment'
+#config.load_incluster_config()
+#v1 = client.AppsV1Api()
 
-# Configuración del cliente de Kubernetes
-config.load_incluster_config()
-v1 = client.AppsV1Api()
+# Configuración de Azure DevOps
+ADO_NAME = os.environ.get('ADO_NAME', 'default_value_if_not_provided')
+ADO_PROJECT = os.environ.get('ADO_PROJECT', 'default_value_if_not_provided')
+ADO_PAT = os.environ.get('ADO_PAT', 'default_value_if_not_provided')
+POOL_NAME = "k8s-cloud"
+POLLING_INTERVAL = 60  # En segundos
+
+
+def get_pool_id():
+    headers = {
+        "Authorization": f"Basic {base64.b64encode(bytes(':' + ADO_PAT, 'utf-8')).decode('ascii')}"
+    }
+
+    url = f"https://dev.azure.com/{ADO_NAME}/_apis/distributedtask/pools?poolName={POOL_NAME}&api-version=6.0"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    return data["value"][0]["id"]
+
+
+def get_running_jobs_for_pool(pool_id):
+    headers = {
+        "Authorization": f"Basic {base64.b64encode(bytes(':' + ADO_PAT, 'utf-8')).decode('ascii')}"
+    }
+
+    url = f"https://dev.azure.com/{ADO_NAME}/_apis/distributedtask/pools/{pool_id}/jobrequests"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+
+    running_jobs = [job for job in data["value"] if "result" not in job]
+    return running_jobs
+
+
+def analyze_jobs(jobs_data):
+    queued_jobs = [job for job in jobs_data if "reservedAgent" not in job]
+    return len(queued_jobs)
+
 
 def get_current_replica_count():
     deployment = v1.read_namespaced_deployment(DEPLOYMENT_NAME, NAMESPACE)
     return deployment.spec.replicas
 
-def needs_scaling(build_data):
-    # Aquí, implementa tu lógica basada en el build_data para decidir si necesitas escalar.
-    return False
+def analyze_jobs(jobs_data):
+    # Lists to store job details
+    running_jobs = []
+    queued_jobs = []
 
-def needs_scaling_down(build_data):
-    # Aquí, implementa tu lógica basada en el build_data para decidir si necesitas reducir el escalado.
-    return False
+    # Iterating over each job to analyze its status and assigned agent
+    for job in jobs_data:
+        job_details = {
+            "requestId": job["requestId"],
+            "jobName": "{:.0f}".format(float(job["owner"]["name"])),
+            "repoName": job["definition"]["name"],
+            "queueTime": job["queueTime"],
+            "status": "In Execution" if "lockedUntil" in job else "Queued"
+        }
+
+        # Check if the job has an agent assigned
+        if "reservedAgent" in job:
+            job_details["agent"] = job["reservedAgent"]["name"]
+            running_jobs.append(job_details)
+        else:
+            job_details["agent"] = "Not Assigned"
+            queued_jobs.append(job_details)
+
+    # Summary of the queue
+    summary = {
+        "totalJobs": len(jobs_data),
+        "runningJobsCount": len(running_jobs),
+        "queuedJobsCount": len(queued_jobs)
+    }
+
+    # Print the results in a colored table
+    print_jobs_table(running_jobs, queued_jobs)
+
+    return running_jobs, queued_jobs, summary
+
+
+def print_jobs_table(running_jobs, queued_jobs):
+    # Merge the lists and sort them by queue time
+    all_jobs = sorted(running_jobs + queued_jobs, key=lambda x: x["queueTime"])
+
+    # Color the rows based on the status
+    colored_rows = []
+    for job in all_jobs:
+        if job["status"] == "In Execution":
+            colored_rows.append([
+                colored(job["requestId"], 'green'),
+                colored(job["jobName"], 'green'),
+                colored(job["repoName"], 'green'),
+                colored(job["queueTime"], 'green'),
+                colored(job["status"], 'green'),
+                colored(job["agent"], 'green')
+            ])
+        elif job["status"] == "Queued":
+            colored_rows.append([
+                colored(job["requestId"], 'yellow'),
+                colored(job["jobName"], 'yellow'),
+                colored(job["repoName"], 'grey'),
+                colored(job["queueTime"], 'yellow'),
+                colored(job["status"], 'yellow'),
+                colored(job["agent"], 'yellow')
+            ])
+        else:
+            colored_rows.append([
+                colored(job["requestId"], 'grey'),
+                colored(job["jobName"], 'grey'),
+                colored(job["repoName"], 'grey'),
+                colored(job["queueTime"], 'grey'),
+                colored(job["status"], 'grey'),
+                colored(job["agent"], 'grey')
+            ])
+
+    # Print the table
+    headers = ["Request ID", "Job Name", "Repo Name", "Queue Time", "Status", "Agent"]
+    print(tabulate(colored_rows, headers=headers, tablefmt='grid'))
+
 
 def scale_up():
     current_replicas = get_current_replica_count()
@@ -36,6 +141,7 @@ def scale_up():
             {"spec": {"replicas": current_replicas + 1}}
         )
         print("Escalado hacia arriba.")
+
 
 def scale_down():
     current_replicas = get_current_replica_count()
@@ -50,17 +156,23 @@ def scale_down():
 
 while True:
     # Paso 1: Obtener datos de Azure DevOps
-    headers = {'Authorization': f'Basic {ADO_PAT}'}
-    response = requests.get(AZDO_URL, headers=headers)
-    build_data = response.json()
+    print("New ")
+    pool_id = get_pool_id()
+    running_jobs_data = get_running_jobs_for_pool(pool_id)
 
-    # Paso 2: Lógica de decisión
-    if needs_scaling(build_data):
-        # Escalar hacia arriba
-        scale_up()
-    elif needs_scaling_down(build_data):
-        # Escalar hacia abajo
-        scale_down()
+    # Imprimimos
+    running_jobs, queued_jobs, summary = analyze_jobs(running_jobs_data)
+    
+    # Paso 2: Analizar trabajos
+    queued_jobs_count = len(queued_jobs)
+
+    # Paso 3: Lógica de decisión
+    if queued_jobs_count > 0:
+        #scale_up()
+        print("toca escalar")
+    else:
+        #scale_down()
+        print("toca desescalar")
 
     # Esperar antes de la siguiente revisión
     time.sleep(POLLING_INTERVAL)
---
+++ pod-orchesta/polling_base.py
@@ -0,0 +1,66 @@
+import requests
+from kubernetes import client, config
+import time
+import os
+
+# Configuración
+ADO_PAT = os.environ.get('ADO_PAT', 'default_value_if_not_provided') 
+AZDO_URL = 'https://dev.azure.com/tu_organizacion/tu_proyecto/_apis/build/builds?api-version=6.0'
+POLLING_INTERVAL = 60  # En segundos
+MAX_REPLICAS = 10
+NAMESPACE = 'tu-namespace'
+DEPLOYMENT_NAME = 'nombre-de-tu-deployment'
+
+# Configuración del cliente de Kubernetes
+config.load_incluster_config()
+v1 = client.AppsV1Api()
+
+def get_current_replica_count():
+    deployment = v1.read_namespaced_deployment(DEPLOYMENT_NAME, NAMESPACE)
+    return deployment.spec.replicas
+
+def needs_scaling(build_data):
+    # Aquí, implementa tu lógica basada en el build_data para decidir si necesitas escalar.
+    return False
+
+def needs_scaling_down(build_data):
+    # Aquí, implementa tu lógica basada en el build_data para decidir si necesitas reducir el escalado.
+    return False
+
+def scale_up():
+    current_replicas = get_current_replica_count()
+    if current_replicas < MAX_REPLICAS:
+        v1.patch_namespaced_deployment_scale(
+            DEPLOYMENT_NAME,
+            NAMESPACE,
+            {"spec": {"replicas": current_replicas + 1}}
+        )
+        print("Escalado hacia arriba.")
+
+def scale_down():
+    current_replicas = get_current_replica_count()
+    if current_replicas > 0:
+        v1.patch_namespaced_deployment_scale(
+            DEPLOYMENT_NAME,
+            NAMESPACE,
+            {"spec": {"replicas": current_replicas - 1}}
+        )
+        print("Escalado hacia abajo.")
+
+
+while True:
+    # Paso 1: Obtener datos de Azure DevOps
+    headers = {'Authorization': f'Basic {ADO_PAT}'}
+    response = requests.get(AZDO_URL, headers=headers)
+    build_data = response.json()
+
+    # Paso 2: Lógica de decisión
+    if needs_scaling(build_data):
+        # Escalar hacia arriba
+        scale_up()
+    elif needs_scaling_down(build_data):
+        # Escalar hacia abajo
+        scale_down()
+
+    # Esperar antes de la siguiente revisión
+    time.sleep(POLLING_INTERVAL)
